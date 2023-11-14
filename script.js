d96e37844c2b56b6712d4f9eec05c5580d3ddeef
dejodammy <95016055+dejodammy@users.noreply.github.com>
2023-11-14 21:39:12 +0100

    update

--- script.js
+++ script.js
@@ -3,24 +3,27 @@ let value;
 let guesses;
 
 function startGame() {
-    numbers = [1,2,3,4,5,6,7,8,9,10]
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     shuffleArray(numbers);
     value = getRandomValue(numbers);
-    guesses = 0;
+    guesses = 3; // Set the maximum number of guesses
 
     document.getElementById("game-container").style.display = "block";
     document.getElementById("message").innerText = "";
-    document.getElementById("range").hiOrLow = "";
+    document.getElementById("range").innerText = "";
     document.getElementById("user-input").value = "";
-    document.getElementById("guesses-left").innerText = "";
+    document.getElementById("guesses-left").innerText = `Guesses left: ${guesses}`;
     document.querySelector("button").style.display = "none";
 }
-function hiOrLow(){
+
+function hiOrLow() {
     const userGuess = parseInt(document.getElementById("user-input").value);
 
     if (!isNaN(userGuess)) {
         if (userGuess > value) {
             document.getElementById("range").innerText = "The value is lower.";
+        } else if (userGuess == value) {
+            document.getElementById("range").innerText = "";
         } else {
             document.getElementById("range").innerText = "The value is higher.";
         }
@@ -28,34 +31,38 @@ function hiOrLow(){
         alert("Please enter a valid number.");
     }
 }
+
 function checkGuess() {
     const userGuess = parseInt(document.getElementById("user-input").value);
 
-    while (!isNaN(userGuess)) {
-        guesses++;
+    if (guesses > 0) {
+        if (!isNaN(userGuess)) {
+            guesses--;
 
-        if (userGuess === value) {
-            document.getElementById("message").innerText = "You got the correct value! Hurray!";
-            document.getElementById("guesses-left").innerText = "";
-            document.querySelector("button").style.display = "block";
-        } else {
-            const guessesLeft = 3 - guesses;
-            document.getElementById("message").innerText = `Wrong guess! Try again. You have ${guessesLeft} guess(es) left.`;
-
-            if (guessesLeft === 0) {
-                document.getElementById("message").innerText = "You ran out of chances. GG's!";
+            if (userGuess === value) {
+                document.getElementById("message").innerText = "You got the correct value! Hurray!";
                 document.getElementById("guesses-left").innerText = "";
                 document.querySelector("button").style.display = "block";
             } else {
-                document.getElementById("guesses-left").innerText = `Guesses left: ${guessesLeft}`;
+                const guessesLeft = guesses;
+                document.getElementById("message").innerText = `Wrong guess! Try again. You have ${guessesLeft} guess(es) left.`;
+
+                if (guessesLeft === 0) {
+                    document.getElementById("message").innerText = "You ran out of chances. GG's!";
+                    document.getElementById("guesses-left").innerText = "";
+                    document.querySelector("button").style.display = "block";
+                } else {
+                    document.getElementById("guesses-left").innerText = `Guesses left: ${guessesLeft}`;
+                }
             }
+        } else {
+            alert("Please enter a valid number.");
         }
-    } 
+    }
 }
 
 function playAgain() {
-    document.getElementById("game-container").style.display = "none";
-    document.querySelector("button").style.display = "none";
+    startGame();
 }
 
 function shuffleArray(array) {
@@ -71,6 +78,7 @@ function getRandomValue(array) {
 
 function handleEnter(event) {
     if (event.key === 'Enter') {
-        submitGuess();
+        checkGuess();
+        hiOrLow();
     }
 }
